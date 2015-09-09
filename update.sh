aef46d6ba95c3a555432dadae53c33acd3aaa3f6
Christopher Lee <leec@chem.ucla.edu>
2015-09-09 14:59:11 -0700

    renamed scripts, added update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this script assumes launch by GUI, in which case PWD already set to repo root
+make update
+echo 'Done.  Press Enter (or click the Close box) to close this window...'
+head -1 >/dev/null
+
+
